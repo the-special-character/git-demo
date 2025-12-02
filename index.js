2533703015010c46384831f7325cbce9be46f79a
Yagnesh Modh <tsc@192.168.1.54>
2025-12-02 13:12:39 +0530

    abcd

---
+++ index.js
@@ -0,0 +1,36 @@
+// const name = "John";
+// const age = 30;
+// const isStudent = true;
+// const hobbies = ["reading", "cooking", "traveling"];
+// const address = {
+//     street: "123 Main St",
+//     city: "Anytown",
+//     state: "CA",
+//     zip: "12345"
+// };
+// const fruits = ["apple", "banana", "cherry"];
+// const vegetables = ["carrot", "broccoli", "spinach"];
+
+const johnInfo = {
+    name: "John",
+    age: 30,
+    isStudent: true,
+    hobbies: ["reading", "cooking", "traveling"],
+    address: {
+        street: "123 Main St",
+        city: "Anytown",
+        state: "CA",
+        zip: "12345"
+    }
+}
+
+const newJohnInfo = {
+    ...johnInfo,
+    address: {
+        ...johnInfo.address,
+        zip: "54321"
+    },
+    age: 31,
+};
+
+console.log(newJohnInfo);
